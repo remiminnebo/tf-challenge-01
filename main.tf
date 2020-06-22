3c0d8f3021d2a2a9acc2b5a4b31fd1582e432435
Remi Minnebo <remi.minnebo@dexmach.com>
2020-06-22 15:37:27 +0200

    first commit

---
+++ main.tf
@@ -0,0 +1,47 @@
+# Configure the Azure provider
+provider "azurerm" { 
+    # The "feature" block is required for AzureRM provider 2.x. 
+    # If you are using version 1.x, the "features" block is not allowed.
+    version = "~>2.0"
+    features {}
+}
+
+resource "azurerm_resource_group" "slotApp1" {
+    name = "rg-tf-challenge-01"
+    location = "westeurope"
+}
+
+resource "azurerm_app_service_plan" "slotApp1" {
+    name                = "asp-tf-challenge-01"
+    location            = azurerm_resource_group.slotApp1.location
+    resource_group_name = azurerm_resource_group.slotApp1.name
+    sku {
+        tier = "Standard"
+        size = "S1"
+    }
+}
+
+resource "azurerm_app_service" "slotApp1" {
+    name                = "apps-tf-challenge-01"
+    location            = azurerm_resource_group.slotApp1.location
+    resource_group_name = azurerm_resource_group.slotApp1.name
+    app_service_plan_id = azurerm_app_service_plan.slotApp1.id
+    app_settings = {
+      "WEBSITE_NODE_DEFAULT_VERSION" = "10.15.2",
+      "ApiUrl" = "",
+      "ApiUrlShoppingCart" = "",
+      "MongoConnectionString" = "",
+      "SqlConnectionString" = "",
+      "productImagesUrl" = "https://raw.githubusercontent.com/microsoft/TailwindTraders-Backend/master/Deploy/tailwindtraders-images/product-detail",
+      "Personalizer__ApiKey" = "",
+      "Personalizer__Endpoint" = ""
+    }
+}
+
+resource "azurerm_app_service_slot" "slotApp1" {
+    name                = "apps-s-tf-challenge-01"
+    location            = azurerm_resource_group.slotApp1.location
+    resource_group_name = azurerm_resource_group.slotApp1.name
+    app_service_plan_id = azurerm_app_service_plan.slotApp1.id
+    app_service_name    = azurerm_app_service.slotApp1.name
+}

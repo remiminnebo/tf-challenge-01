b71359e2e4111aa860e433f0d1f928010b9ea042
Remi Minnebo <remi.minnebo@dexmach.com>
2020-06-22 22:37:16 +0200

    We're getting there baby

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ provider "azurerm" {
     client_id       = "${var.client_id}"
     client_secret   = "${var.client_secret}"
     tenant_id       = "${var.tenant_id}"
+    features{}
 }
 
 resource "azurerm_resource_group" "slotApp1" {

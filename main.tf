5727680b3e6b927e316c864780f6996a9f1c3102
Remi Minnebo <remi.minnebo@dexmach.com>
2020-06-22 22:33:54 +0200

    added some parentheses bitch

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
     # The "feature" block is required for AzureRM provider 2.x. 
     # If you are using version 1.x, the "features" block is not allowed.
     version = "~>2.0"
-    subscription_id = var.sub
+    subscription_id = "${var.sub}"
     client_id       = "${var.client_id}"
     client_secret   = "${var.client_secret}"
     tenant_id       = "${var.tenant_id}"

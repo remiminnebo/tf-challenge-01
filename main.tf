8b2a20342fedce57f34c1f172358a178a0c7ce45
Remi Minnebo <remi.minnebo@dexmach.com>
2020-06-22 21:47:56 +0200

    added some vars in maintf

--- main.tf
+++ main.tf
@@ -3,7 +3,10 @@ provider "azurerm" {
     # The "feature" block is required for AzureRM provider 2.x. 
     # If you are using version 1.x, the "features" block is not allowed.
     version = "~>2.0"
-    features {}
+    subscription_id = var.sub
+    client_id       = "${var.client_id}"
+    client_secret   = "${var.client_secret}"
+    tenant_id       = "${var.tenant_id}"
 }
 
 resource "azurerm_resource_group" "slotApp1" {
